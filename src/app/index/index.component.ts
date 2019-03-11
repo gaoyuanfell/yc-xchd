938aba40c5e98e7d0e242aae2544c91337759fe0
moka <gaoyuanfell@sina.com>
2019-03-11 18:27:54 +0800

    1

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.less']
+  styleUrls: ['./index.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IndexComponent implements OnInit {
 
